97297fff7e28903d62e696c2e8e6c25fe4325c2e
StormBlue <gao2808941@gmail.com>
2017-01-16 17:31:42 +0800

    [FEATURE]1.test BottomSheetBehavior;

--- app/src/main/java/com/bluestrom/gao/explorersouhupics/activity/MainActivity.java
+++ app/src/main/java/com/bluestrom/gao/explorersouhupics/activity/MainActivity.java
@@ -3,10 +3,13 @@ package com.bluestrom.gao.explorersouhupics.activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.bluestrom.gao.explorersouhupics.R;
 import com.bluestrom.gao.explorersouhupics.adapter.MainContentViewPagerAdapter;
@@ -16,7 +19,14 @@ import com.bluestrom.gao.explorersouhupics.fragment.dummy.DummyContent;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, FunnyFragment.OnListFragmentInteractionListener {
+import static android.support.design.widget.BottomSheetBehavior.STATE_COLLAPSED;
+import static android.support.design.widget.BottomSheetBehavior.STATE_EXPANDED;
+import static android.support.design.widget.BottomSheetBehavior.STATE_HIDDEN;
+import static android.support.v4.widget.ViewDragHelper.STATE_SETTLING;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, FunnyFragment.OnListFragmentInteractionListener, View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     private ViewPager contentViewPager;
 
@@ -25,6 +35,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     private BottomNavigationView navigationView;
 
     private List<Fragment> fragmentList;
+
+    private BottomSheetBehavior mBottomSheetBehavior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,6 +96,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         navigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_view);
         navigationView.setOnNavigationItemSelectedListener(this);
         contentViewPager = (ViewPager) findViewById(R.id.main_content_viewpager);
+        mBottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_nav_view));
     }
 
 
@@ -105,5 +118,22 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public void onListFragmentInteraction(DummyContent.PhotoBean item) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.test_button:
+                Log.i(TAG, "操作开始SheetBehavior.State" + mBottomSheetBehavior.getState());
+                if (mBottomSheetBehavior.getState() == STATE_EXPANDED) {
+                    mBottomSheetBehavior.setState(STATE_COLLAPSED);
+                } else if (mBottomSheetBehavior.getState() == STATE_COLLAPSED) {
+                    mBottomSheetBehavior.setState(STATE_EXPANDED);
+                }
+                Log.i(TAG, "操作结束SheetBehavior.State" + mBottomSheetBehavior.getState());
+                break;
+            default:
+                break;
+        }
     }
 }
