c53c14f6bea9e3bb857ec625b39f4ac70cfc0d5f
gaohaitao <gao2808941@gmail.com>
2017-01-18 01:02:27 +0800

    [FEATURE]1.Test nestedscrollview;

--- app/src/main/java/com/bluestrom/gao/explorersouhupics/fragment/FunnyFragment.java
+++ app/src/main/java/com/bluestrom/gao/explorersouhupics/fragment/FunnyFragment.java
@@ -2,6 +2,7 @@ package com.bluestrom.gao.explorersouhupics.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
